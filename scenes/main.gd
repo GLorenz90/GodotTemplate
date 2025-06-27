e22e0cefa7b9966e6dd8b77148e0d910993c2e8d
Grayson Lorenz <graysonc.l.lorenz@gmail.com>
2025-06-27 01:06:29 -0500

    movement setup

--- scenes/main.gd
+++ scenes/main.gd
@@ -1,30 +1,40 @@
 extends Node2D
 class_name Main
 
-var p1_input_buffer = {
-  "p1_char_h": 0.0,
-  "p1_char_v": 0.0,
-  "p1_char_attack_pressed": false,
-  "p1_char_attack_held": false,
-  "p1_char_attack_held_time": 0.0,
-  "p1_char_special_pressed": false,
-  "p1_char_special_held": false,
-  "p1_char_special_held_time": 0.0,
-  "p1_char_jump_pressed": false,
-  "p1_char_jump_held": false,
-  "p1_char_jump_held_time": 0.0,
-  "p1_char_dash_pressed": false,
-  "p1_char_dash_held": false,
-  "p1_char_dash_held_time": 0.0,
-  "p1_char_interact_pressed": false,
-  "p1_char_interact_held": false,
-  "p1_char_interact_held_time": 0.0
+var debugLabel;
+
+const templateInputBuffer := {
+  "char_h": 0.0,
+  "char_v": 0.0,
+  "char_vector": Vector2(0.0, 0.0),
+  "char_attack_pressed": false,
+  "char_attack_held": false,
+  "char_attack_held_time": 0.0,
+  "char_special_pressed": false,
+  "char_special_held": false,
+  "char_special_held_time": 0.0,
+  "char_jump_pressed": false,
+  "char_jump_held": false,
+  "char_jump_held_time": 0.0,
+  "char_dash_pressed": false,
+  "char_dash_held": false,
+  "char_dash_held_time": 0.0,
+  "char_interact_pressed": false,
+  "char_interact_held": false,
+  "char_interact_held_time": 0.0
   #TODO: p2 controls need to be added to the project
 };
+
+var p1InputBuffer := templateInputBuffer;
+var p2InputBuffer := templateInputBuffer;
 
 func _init():
   Global.main = self;
 # end _init
+
+func _ready() -> void:
+  debugLabel = $SubViewportContainer/SubViewport/SceneParent/Label;
+# end _ready
 
 func _process(delta: float) -> void:
   process_inputs(delta);
@@ -32,25 +42,45 @@ func _process(delta: float) -> void:
 
 func process_inputs(delta) -> void:
   if(Global.p1_char != null):
-    p1_input_buffer = {
-      "p1_char_h": Input.get_axis("p1_char_left", "p1_char_right"),
-      "p1_char_v": Input.get_axis("p1_char_down", "p1_char_up"),
-      "p1_char_attack_pressed": Input.is_action_just_pressed("p1_char_attack"),
-      "p1_char_attack_held": Input.is_action_pressed("p1_char_attack"),
-      "p1_char_attack_held_time": p1_input_buffer["p1_char_attack_held_time"] + delta if Input.is_action_pressed("p1_char_attack") else 0.0,
-      "p1_char_special_pressed": Input.is_action_just_pressed("p1_char_special"),
-      "p1_char_special_held": Input.is_action_pressed("p1_char_special"),
-      "p1_char_special_held_time": p1_input_buffer["p1_char_special_held_time"] + delta if Input.is_action_pressed("p1_char_special") else 0.0,
-      "p1_char_jump_pressed": Input.is_action_just_pressed("p1_char_jump"),
-      "p1_char_jump_held": Input.is_action_pressed("p1_char_jump"),
-      "p1_char_jump_held_time": p1_input_buffer["p1_char_jump_held_time"] + delta if Input.is_action_pressed("p1_char_jump") else 0.0,
-      "p1_char_dash_pressed": Input.is_action_just_pressed("p1_char_dash"),
-      "p1_char_dash_held": Input.is_action_pressed("p1_char_dash"),
-      "p1_char_dash_held_time": p1_input_buffer["p1_char_dash_held_time"] + delta if Input.is_action_pressed("p1_char_dash") else 0.0,
-      "p1_char_interact_pressed": Input.is_action_just_pressed("p1_char_interact"),
-      "p1_char_interact_held": Input.is_action_pressed("p1_char_interact"),
-      "p1_char_interact_held_time": p1_input_buffer["p1_char_interact_held_time"] + delta if Input.is_action_pressed("p1_char_interact") else 0.0
-      #TODO: p2 controls need to be added to the project
+    p1InputBuffer = {
+      "char_h": Input.get_axis("p1_char_left", "p1_char_right"),
+      "char_v": Input.get_axis("p1_char_down", "p1_char_up"),
+      "char_vector": Vector2(Input.get_axis("p1_char_left", "p1_char_right"), Input.get_axis("p1_char_down", "p1_char_up")),
+      "char_attack_pressed": Input.is_action_just_pressed("p1_char_attack"),
+      "char_attack_held": Input.is_action_pressed("p1_char_attack"),
+      "char_attack_held_time": p1InputBuffer["char_attack_held_time"] + delta if Input.is_action_pressed("p1_char_attack") else 0.0,
+      "char_special_pressed": Input.is_action_just_pressed("p1_char_special"),
+      "char_special_held": Input.is_action_pressed("p1_char_special"),
+      "char_special_held_time": p1InputBuffer["char_special_held_time"] + delta if Input.is_action_pressed("p1_char_special") else 0.0,
+      "char_jump_pressed": Input.is_action_just_pressed("p1_char_jump"),
+      "char_jump_held": Input.is_action_pressed("p1_char_jump"),
+      "char_jump_held_time": p1InputBuffer["char_jump_held_time"] + delta if Input.is_action_pressed("p1_char_jump") else 0.0,
+      "char_dash_pressed": Input.is_action_just_pressed("p1_char_dash"),
+      "char_dash_held": Input.is_action_pressed("p1_char_dash"),
+      "char_dash_held_time": p1InputBuffer["char_dash_held_time"] + delta if Input.is_action_pressed("p1_char_dash") else 0.0,
+      "char_interact_pressed": Input.is_action_just_pressed("p1_char_interact"),
+      "char_interact_held": Input.is_action_pressed("p1_char_interact"),
+      "char_interact_held_time": p1InputBuffer["char_interact_held_time"] + delta if Input.is_action_pressed("p1_char_interact") else 0.0
     };
   # end if
+  #debugLabel.text = "Debug:" \
+#+ "\n------ Inputs P1 ------" \
+#+ "\nchar_h: " + str(p1InputBuffer["char_h"]) \
+#+ "\nchar_v: " + str(p1InputBuffer["char_v"]) \
+#+ "\nchar_vector: " + str(p1InputBuffer["char_vector"]) \
+#+ "\nchar_attack_pressed: " + str(p1InputBuffer["char_attack_pressed"]) \
+#+ "\nchar_attack_held: " + str(p1InputBuffer["char_attack_held"]) \
+#+ "\nchar_attack_held_time: " + str(p1InputBuffer["char_attack_held_time"]) \
+#+ "\nchar_special_pressed: " + str(p1InputBuffer["char_special_pressed"]) \
+#+ "\nchar_special_held: " + str(p1InputBuffer["char_special_held"]) \
+#+ "\nchar_special_held_time: " + str(p1InputBuffer["char_special_held_time"]) \
+#+ "\nchar_jump_pressed: " + str(p1InputBuffer["char_jump_pressed"]) \
+#+ "\nchar_jump_held: " + str(p1InputBuffer["char_jump_held"]) \
+#+ "\nchar_jump_held_time: " + str(p1InputBuffer["char_jump_held_time"]) \
+#+ "\nchar_dash_pressed: " + str(p1InputBuffer["char_dash_pressed"]) \
+#+ "\nchar_dash_held: " + str(p1InputBuffer["char_dash_held"]) \
+#+ "\nchar_dash_held_time: " + str(p1InputBuffer["char_dash_held_time"]) \
+#+ "\nchar_interact_pressed: " + str(p1InputBuffer["char_interact_pressed"]) \
+#+ "\nchar_interact_held: " + str(p1InputBuffer["char_interact_held"]) \
+#+ "\nchar_interact_held_time: " + str(p1InputBuffer["char_interact_held_time"])
 # end process_inputs
--- scenes/player/player_character.gd
+++ scenes/player/player_character.gd
@@ -1,14 +1,314 @@
 extends CharacterBody2D
 class_name PlayerCharacter
 
-@export var SPEED := 500.0;
-@export var JUMP_VELOCITY := 400.0;
-@export var MAX_SPEED := 1000;
-@export var MAX_COYOTE_TIME := 0.2;
+#region CONSTANT VARIABLES =========================================================================
+enum CHAR_STATES {
+  INIT,
+  IDLE,
+  RUNNING,
+  DASHING,
+  JUMPING,
+  FALLING,
+  WALL_SLIDING,
+  CLIMBING,
+  ATTACKING,
+  HURT
+}
 
-func _init() -> void:
+const LERP_SPEED = 10.0;
+
+const fallImg = preload("res://images/character/WolfFall.png");
+const hurtImg = preload("res://images/character/WolfHurt.png");
+const idleImg = preload("res://images/character/WolfIdle.png");
+const jumpImg = preload("res://images/character/WolfJump.png");
+const runImg = preload("res://images/character/WolfRun.png");
+const slash1Img = preload("res://images/character/WolfSlash1.png");
+const slash2Img = preload("res://images/character/WolfSlash2.png");
+const wallSlideImg = preload("res://images/character/WolfWallSlide.png");
+const dashImg = preload("res://images/character/WolfDash.png");
+#endregion
+
+#region COMPUTED VARIABLES =========================================================================
+var isP1 := false;
+var isP2 := false;
+var inputData := Global.main.templateInputBuffer;
+var state: CHAR_STATES = CHAR_STATES.IDLE;
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity");
+var isDashSpeed := false;
+var coyoteTimeRemaining = CharStats.MAX_COYOTE_TIME;
+#endregion
+
+#region BUILT IN FUNCTIONS =========================================================================
+func _ready() -> void:
   if(Global.p1_char == null):
     Global.p1_char = self;
+    isP1 = true;
   elif(Global.p2_char == null):
     Global.p2_char = self;
+    isP2 = true;
+  # end if
+  
+  $DashTimer.wait_time = CharStats.GET_TOTAL_DASH_TIME();
+  $JumpTimer.wait_time = CharStats.GET_TOTAL_JUMP_TIME();
 # end _init
+
+func _process(delta: float) -> void:
+  Global.main.debugLabel.text = str(str(CHAR_STATES.keys()[state]));
+  Global.main.debugLabel.text += "\n" + str(velocity);
+  
+  if(is_on_floor()):
+    resetJumpFlags();
+  elif(coyoteTimeRemaining > 0.0):
+    coyoteTimeRemaining -= delta;
+  updateSprite();
+  setInputData();
+  processState(delta);
+  
+  move_and_slide();
+# end _process
+#endregion
+
+#region STATE FUNCTIONS ============================================================================
+func processState(delta) -> void:
+  match(state):
+    CHAR_STATES.INIT:
+      doInitState(delta);
+    CHAR_STATES.IDLE:
+      doIdleState(delta);
+    CHAR_STATES.RUNNING:
+      doRunningState(delta);
+    CHAR_STATES.DASHING:
+      doDashingState(delta);
+    CHAR_STATES.JUMPING:
+      doJumpingState(delta);
+    CHAR_STATES.FALLING:
+      doFallingState(delta);
+    CHAR_STATES.WALL_SLIDING:
+      doWallSlidingState(delta);
+    CHAR_STATES.CLIMBING:
+      doClimbingState(delta);
+    CHAR_STATES.ATTACKING:
+      doAttackingState(delta);
+    CHAR_STATES.HURT:
+      doHurtState(delta);
+  #end match
+#end processState
+
+func doInitState(delta) -> void:
+  #TODO: Intro animation
+  state = CHAR_STATES.IDLE;
+# end doInitState
+
+func doIdleState(delta) -> void:
+  updateHorizontalVelocity(delta);
+  applyGravity(delta);
+  
+  neutralStateCheck();
+  moveStateCheck();
+# end doInitState
+
+func doRunningState(delta) -> void:
+  updateHorizontalVelocity(delta);
+  applyGravity(delta);
+  
+  if(roundf(velocity.x) == 0.0):
+    neutralStateCheck();
+  else:
+    moveStateCheck();
+# end doInitState
+
+func doDashingState(delta) -> void:
+  if($DashTimer.is_stopped()):
+    $DashTimer.start();
+    
+  var dashDirection = inputData["char_h"] if inputData["char_h"] != 0 else $Sprite.scale.x;
+  velocity.x = CharStats.GET_TOTAL_DASH_SPEED() * dashDirection;
+  
+  # if we push a direction we aren't dashing toward, stop dashing
+  var isInputInverseOfDirection: bool = (inputData["char_h"] && ((inputData["char_h"] > 0 && $Sprite.scale.x < 0) || (inputData["char_h"] < 0 && $Sprite.scale.x > 0)));
+  if (isInputInverseOfDirection || state != CHAR_STATES.DASHING):
+    if(!$DashTimer.is_stopped()):
+      $DashTimer.stop();
+    # end if
+    neutralStateCheck();
+  elif (isJumpBuffered() && canJump()):
+    state = CHAR_STATES.JUMPING;
+  #end if
+# end doInitState
+
+func doJumpingState(delta) -> void:
+  if($JumpTimer.is_stopped()):
+    $JumpTimer.start();
+    if(inputData["char_dash_held"]):
+      isDashSpeed = true;
+  velocity.y = CharStats.GET_TOTAL_JUMP_VELOCITY() * -1;
+  updateHorizontalVelocity(delta);
+  
+  if(!inputData["char_jump_held"]):
+    if(!$JumpTimer.is_stopped()):
+      $JumpTimer.stop();
+    # end if
+    neutralStateCheck();
+# end doInitState
+
+func doFallingState(delta) -> void:
+  updateHorizontalVelocity(delta);
+  applyGravity(delta);
+  
+  if (isJumpBuffered() && canJump()):
+    state = CHAR_STATES.JUMPING;
+  else:
+    neutralStateCheck();
+# end doFallingState
+
+func doWallSlidingState(delta) -> void:
+  resetJumpFlags();
+  var lastCollision = get_last_slide_collision();
+  var lastCollisionDirection = 0;
+  if(lastCollision):
+    lastCollisionDirection = sign(lastCollision.get_position().x - global_position.x);
+    if(isJumpBuffered()):
+      state = CHAR_STATES.JUMPING;
+      velocity.x += CharStats.GET_TOTAL_WALK_SPEED() * lastCollisionDirection * -1;
+      return
+    # end if
+  # end if
+  
+  var isInputTowardWall: bool = (is_on_wall_only() && lastCollisionDirection == sign(inputData["char_h"]));
+  if (isInputTowardWall):
+    velocity.y = CharStats.GET_TOTAL_WALL_SLIDE_VELOCITY();
+  else:
+    if(is_on_floor()):
+      state = CHAR_STATES.IDLE;
+    else:
+      state = CHAR_STATES.FALLING;
+# end doSlidingState
+
+func doClimbingState(delta) -> void:
+  updateVerticalVelocity(delta);
+#end doClimbingState
+
+func doAttackingState(delta) -> void:
+  pass;
+# end doAttackingState
+
+func doHurtState(delta) -> void:
+  pass;
+# end doAttackingState
+
+func neutralStateCheck() -> void:
+  if(state != CHAR_STATES.IDLE && is_on_floor()):
+    state = CHAR_STATES.IDLE;
+  elif(state != CHAR_STATES.WALL_SLIDING && is_on_wall() && inputData["char_h"] != 0.0):
+    state = CHAR_STATES.WALL_SLIDING;
+  elif(state != CHAR_STATES.FALLING && !is_on_floor() && !(is_on_wall() && inputData["char_h"] != 0.0)):
+    state = CHAR_STATES.FALLING;
+# end idleFallOrSlideStateCheck
+
+func moveStateCheck() -> void:
+  if(state != CHAR_STATES.DASHING && inputData["char_dash_pressed"] && is_on_floor()):
+    state = CHAR_STATES.DASHING;
+  elif(state != CHAR_STATES.JUMPING && isJumpBuffered() && canJump()):
+    state = CHAR_STATES.JUMPING;
+  elif(state != CHAR_STATES.RUNNING && inputData["char_h"] != 0 && is_on_floor()):
+    state = CHAR_STATES.RUNNING;
+  elif(state != CHAR_STATES.FALLING && !is_on_floor()):
+    state = CHAR_STATES.FALLING;
+# end moveStateCheck
+#endregion
+
+#region SIGNALS ====================================================================================
+func _on_dash_timer_timeout() -> void:
+  neutralStateCheck();
+# end _on_dash_timer_timeout
+
+func _on_jump_timer_timeout() -> void:
+  neutralStateCheck();
+# end _on_jump_timer_timeout
+#endregion
+
+#region UTILITY FUNCTIONS ==========================================================================
+func setInputData() -> void:
+  if(Global.p1_char != null):
+    inputData = Global.main.p1InputBuffer;
+  elif(Global.p2_char != null):
+    inputData = Global.main.p2InputBuffer;
+# end setInputData
+
+func updateHorizontalVelocity(delta) -> void:
+  if (inputData["char_h"]):
+    velocity.x = move_toward(velocity.x, inputData["char_h"] * (CharStats.GET_TOTAL_DASH_SPEED() if isDashSpeed else CharStats.GET_TOTAL_WALK_SPEED()), CharStats.GET_TOTAL_WALK_SPEED() * delta * LERP_SPEED); 
+    #velocity.x = inputData["char_h"] * CharStats.GET_TOTAL_WALK_SPEED();
+  else:
+    velocity.x = move_toward(velocity.x, 0, CharStats.GET_TOTAL_WALK_SPEED() * delta * LERP_SPEED);
+    #velocity.x = 0;
+  #end if
+# end updateVelocity
+
+func updateVerticalVelocity(delta) -> void:
+  if (inputData["char_v"]):
+    velocity.y = inputData["char_v"] * delta * CharStats.GET_TOTAL_WALK_SPEED() * -1;
+  else:
+    velocity.y = 0;
+  #end if
+# end updateVelocity
+
+func applyGravity(delta) -> void:
+  velocity.y = min(CharStats.MAX_VELOCITY, velocity.y + (gravity * delta * 2))
+# end applyGravity
+
+func canJump() -> bool:
+  return is_on_floor() || coyoteTimeRemaining > 0;
+# end canJump
+
+func isJumpBuffered() -> bool:
+  return inputData["char_jump_pressed"] || (inputData["char_jump_held"] && inputData["char_jump_held_time"] <= CharStats.MAX_JUMP_BUFFER_TIME);
+# end isJumpBuffered
+
+func resetJumpFlags() -> void:
+  isDashSpeed = false;
+  coyoteTimeRemaining = CharStats.MAX_COYOTE_TIME;
+# end resetJumpFlags
+#endregion
+
+#region SPRITE FUNCTIONS ===========================================================================
+func updateSprite() -> void:
+  if velocity.x > 0:
+    $Sprite.scale.x = 1;
+  #end if
+  if velocity.x < 0:
+    $Sprite.scale.x = -1;
+  #end if
+  if velocity.x == 0 && inputData["char_h"] != 0:
+    $Sprite.scale.x = roundi(inputData["char_h"]);
+    
+  match(state):
+    CHAR_STATES.INIT:
+      $Sprite.texture = idleImg;
+    CHAR_STATES.IDLE:
+      $Sprite.texture = idleImg;
+    CHAR_STATES.RUNNING:
+      if(velocity.x == 0.0):
+        $Sprite.texture = idleImg;
+      else:
+        $Sprite.texture = runImg;
+    CHAR_STATES.DASHING:
+      $Sprite.texture = dashImg;
+    CHAR_STATES.JUMPING:
+      $Sprite.texture = jumpImg;
+    CHAR_STATES.FALLING:
+      if(velocity.y > 0.0):
+        $Sprite.texture = fallImg;
+      else:
+        $Sprite.texture = jumpImg;
+    CHAR_STATES.WALL_SLIDING:
+      $Sprite.texture = wallSlideImg;
+    CHAR_STATES.CLIMBING:
+      $Sprite.texture = idleImg;
+    CHAR_STATES.ATTACKING:
+      $Sprite.texture = slash1Img;
+    CHAR_STATES.HURT:
+      $Sprite.texture = hurtImg;
+  #end match
+# END updateSprite
+#endregion
